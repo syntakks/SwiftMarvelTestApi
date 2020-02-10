d534dc6a3ba2a35e8a5d16636ffde60cefb7888c
Steve Wall <syntakks@gmail.com>
2020-02-10 10:51:52 -0600

    Initial Commit
    First Commit.

---
+++ MarvelAPITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelAPITest
+//
+//  Created by Stephen Wall on 2/10/20.
+//  Copyright © 2020 Stephen Wall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
